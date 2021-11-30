d4e30e12aba5f770836150f1bbd91a6f0f6f0de5
fujino-fpu <95068422+fujino-fpu@users.noreply.github.com>
2021-11-30 11:33:05 +0900

    ヘッドレスにするのと、カメラとマイクの許可を自動であたえるのと。

--- headlessExperimentRoom.py
+++ headlessExperimentRoom.py
@@ -4,14 +4,28 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.chrome.options import Options
+
+option = Options()
+
+option.add_argument("--disable-infobars")
+option.add_argument("start-maximized")
+option.add_argument("--disable-extensions")
+option.add_argument('-headless')
+
+# Pass the argument 1 to allow and 2 to block
+option.add_experimental_option("prefs", { 
+    "profile.default_content_setting_values.media_stream_mic": 1, 
+    "profile.default_content_setting_values.media_stream_camera": 1, 
+    "profile.default_content_setting_values.notifications": 1 
+})
 
 url = "https://fujino-fpu.github.io/Experimental/ExperimentRoom2Ver3test.html"
 
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+driver = webdriver.Chrome(chrome_options=option,executable_path="/usr/bin/chromedriver")
 
 driver.get(url)
 
-# 画面表示を最大5秒まで待つ
 driver.implicitly_wait(5)
 
 cam = driver.find_element_by_id("VideoList")
@@ -25,9 +39,7 @@ select_mic.select_by_index(0)
 button_start = driver.find_element_by_id("local-Start")
 button_start.click()
 
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 30)
 button_join = wait.until(expected_conditions.element_to_be_clickable((By.ID, "make-call")))
 
 button_join.click()
-
-
